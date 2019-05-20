3a6953b172a1ab8ba5002459e993fab4d30f7dc1
Hoang Nguyen <hoang@wetrust.io>
2019-05-20 16:24:45 -0700

    init MsgCreateWallet

---
+++ x/multisigservice/msgs.go
@@ -0,0 +1,55 @@
+package multisigservice
+
+import (
+	"encoding/json"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+// MsgCreateWallet defines a CreateWallet message
+type MsgCreateWallet struct {
+	Creator            sdk.AccAddress
+	Owners             []sdk.AccAddress
+	RequiredSignatures sdk.Int
+}
+
+// Route should return the name of the module
+func (msg MsgCreateWallet) Route() string {
+	return "multisigservice"
+}
+
+// Type should return the action
+func (msg MsgCreateWallet) Type() string {
+	return "createwallet"
+}
+
+// ValidateBasic runs stateless checks on the message
+func (msg MsgCreateWallet) ValidateBasic() sdk.Error {
+	if msg.Creator.Empty() {
+		return sdk.ErrInvalidAddress(msg.Creator.String())
+	}
+
+	if msg.RequiredSignatures.LT(sdk.OneInt()) {
+		return sdk.ErrUnknownRequest("RequiredSignatures must be equal or greater than 1.")
+	}
+
+	if msg.RequiredSignatures.GT(sdk.NewInt(int64(len(msg.Owners)))) {
+		return sdk.ErrUnknownRequest("RequiredSignatures must be equal or less than Owners.length.")
+	}
+
+	return nil
+}
+
+// GetSignBytes encodes the message for signing
+func (msg MsgCreateWallet) GetSignBytes() []byte {
+	b, err := json.Marshal(msg)
+	if err != nil {
+		panic(err)
+	}
+	return sdk.MustSortJSON(b)
+}
+
+// GetSigners defines whose signature is required which is the Creator
+func (msg MsgCreateWallet) GetSigners() []sdk.AccAddress {
+	return []sdk.AccAddress{msg.Creator}
+}
