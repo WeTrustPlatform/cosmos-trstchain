e20256336d211c85844e28b5a982bd8b756dce91
Hoang Nguyen <hoang@wetrust.io>
2019-05-24 13:48:22 -0700

    add test for sequence 0

--- x/multisigservice/keeper_test.go
+++ x/multisigservice/keeper_test.go
@@ -7,18 +7,21 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestDeriveAccAddressSuccess(t *testing.T) {
-	var addr, expected, actual types.AccAddress
+func getAccAddressFromBech32(addr string) (accAddr types.AccAddress) {
 	var err error
-	addr, err = types.AccAddressFromBech32("cosmos19a99vt09df3vmtml95ylc2u6n532hv8mt9drjy")
+	accAddr, err = types.AccAddressFromBech32(addr)
 	if err != nil {
 		panic(err)
 	}
-	expected, err = types.AccAddressFromBech32("cosmos1kkj38xqjeezyvk3cyghjldh408aryumw2n6jey")
-	if err != nil {
-		panic(err)
-	}
+	return accAddr
+}
+
+func TestDeriveAccAddressSuccess(t *testing.T) {
+	addr := getAccAddressFromBech32("cosmos19a99vt09df3vmtml95ylc2u6n532hv8mt9drjy")
+
+	actual := DeriveAccAddress(addr, 0)
+	require.Equal(t, getAccAddressFromBech32("cosmos19svpfaujcn5vqvgqcylwvakq9xeh7vnx8ufr00"), actual)
 
 	actual = DeriveAccAddress(addr, 1)
-	require.Equal(t, expected, actual)
+	require.Equal(t, getAccAddressFromBech32("cosmos1kkj38xqjeezyvk3cyghjldh408aryumw2n6jey"), actual)
 }
