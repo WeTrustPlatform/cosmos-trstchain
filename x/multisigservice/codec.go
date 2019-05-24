fe24a7c72e9ef6d2ec776c7052c3837e7385d2fb
Hoang Nguyen <hoang@wetrust.io>
2019-05-24 16:12:11 -0700

    add multisigservice codec

---
+++ x/multisigservice/codec.go
@@ -0,0 +1,10 @@
+package multisigservice
+
+import (
+	"github.com/cosmos/cosmos-sdk/codec"
+)
+
+// RegisterCodec registers concrete types on the Amino codec
+func RegisterCodec(cdc *codec.Codec) {
+	cdc.RegisterConcrete(MsgCreateWallet{}, "multisigservice/CreateWallet", nil)
+}
